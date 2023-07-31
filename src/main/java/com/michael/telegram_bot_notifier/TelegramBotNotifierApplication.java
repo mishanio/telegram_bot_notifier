7b578855ebc7bd532f35581b9202678b0ab7d4dd
Michel <michaelrocksuperstar@gmail.com>
2023-07-31 09:20:26 +0300

    init

---
+++ src/main/java/com/michael/telegram_bot_notifier/TelegramBotNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.michael.telegram_bot_notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michael/telegram_bot_notifier/TelegramBotNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.telegram_bot_notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
