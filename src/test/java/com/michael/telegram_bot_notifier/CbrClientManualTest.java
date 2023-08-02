da8593fab43530ee5c5d034af62fbc5375aa124f
Michel <michaelrocksuperstar@gmail.com>
2023-08-02 09:45:21 +0300

    add client manual test?

--- src/test/java/com/michael/telegram_bot_notifier/CbrClientManualTest.java
+++ src/test/java/com/michael/telegram_bot_notifier/CbrClientManualTest.java
@@ -11,23 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {CbrClientManualTest.TestConfig.class, CbrClientConfig.class})
+@ContextConfiguration(classes = {CbrClientConfig.class})
+@TestPropertySource("/application.properties")
 public class CbrClientManualTest {
-
-  @Configuration
-  static class TestConfig {
-      @Bean
-      public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
-          PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-          propertySourcesPlaceholderConfigurer.setPropertySources(new  );
-          return propertySourcesPlaceholderConfigurer;
-      }
-  }
 
     @Autowired
     public CbrClient client;
